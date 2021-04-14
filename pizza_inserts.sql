9ae313d47912087d5a7466b54bfebed36aac5901
Lidia <230459@edu.p.lodz.pl>
2021-04-14 11:45:10 +0200

    menu table done

---
+++ pizza_inserts.sql
@@ -0,0 +1,16 @@
+INSERT INTO menu VALUES
+('americano', 7.40, 'U.S', 'wholemeal'),
+('frutti di mare', 9.20, 'Italy', 'wholemeal'),
+('funghi', 7.30, 'Italy', 'wholemeal'),
+('garlic', 3.50, null, 'wholemeal'),
+('ham', 7.30, null, 'white flour'),
+('hawaiian', 7.40, 'Canada', 'wholemeal'),
+('margherita', 6.20, 'Italy', 'white flour'),
+('mexicano', 7.40, 'Mexico', 'white flour'),
+('napoletana', 7.40, 'Italy', 'white flour'),
+('quattro stagioni', 7.80, 'Italy', 'wholemeal'),
+('siciliano', 7.40, 'Italy', 'wholemeal'),
+('special', 9.90, null, 'white flour'),
+('vegetarian', 7.40, null, 'wholemeal'),
+('viennese', 7.40, 'Italy', 'white flour')
+;
