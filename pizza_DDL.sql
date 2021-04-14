0708fbff84ebad0c4bf68ccee5e20742747bfae2
Lidia <230459@edu.p.lodz.pl>
2021-04-14 11:10:50 +0200

    tables creation

---
+++ pizza_DDL.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE pizza;
+USE pizza;
+
+CREATE TABLE items(
+	ingredient varchar(12) CONSTRAINT items_PK PRIMARY KEY,
+	type varchar(8)
+	);
+
+CREATE TABLE menu(
+	pizza varchar(30) CONSTRAINT menu_PK PRIMARY KEY,
+	price dec(4,2),
+	country varchar(20),
+	base varchar(20)
+	);
+
+CREATE TABLE recipe(
+	pizza varchar(30) FOREIGN KEY REFERENCES menu(pizza),
+	ingredient varchar(12) FOREIGN KEY REFERENCES items(ingredient),
+	amount int
+	);
