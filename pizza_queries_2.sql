97094897def232b30aae9131b132c3efbbd17fd7
Lidia <230459@edu.p.lodz.pl>
2021-04-22 18:02:49 +0200

    updated

--- pizza_queries_2.sql
+++ pizza_queries_2.sql
@@ -1,17 +1,15 @@
 -- Task 1
 
 --4
-SELECT SUM(price)
+SELECT SUM(price) AS margherita_vegetarian
 FROM menu
-WHERE pizza='margherita' OR pizza='vegetarian'
+WHERE pizza='margherita' OR pizza='vegetarian';
 
 --5
-(SELECT MIN(price)
-FROM menu) UNION
-(SELECT MAX(price)
-FROM menu) UNION
-(SELECT AVG(price)
-FROM menu)
+SELECT MIN(price) AS min_price, 
+MAX(price) AS max_price, 
+AVG(price) AS avg_price
+FROM menu;
 
 -- Task 2
 
