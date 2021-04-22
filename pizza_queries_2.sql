5afe5a58674dd1e72df2c8d2088e6cd87a6762b5
odpaleniek1337 <deadlodz@wp.pl>
2021-04-22 19:34:40 +0200

    6,7,8 from task 1 git, 6,7,8 from task2 not so sure

--- pizza_queries_2.sql
+++ pizza_queries_2.sql
@@ -11,6 +11,20 @@ MAX(price) AS max_price,
 AVG(price) AS avg_price
 FROM menu;
 
+--6
+SELECT COUNT(base) as 'No. of wholemeal'
+FROM menu
+WHERE base='wholemeal'
+
+--7
+SELECT COUNT(1) as 'No. of no country'
+FROM menu
+WHERE country IS NULL
+
+--8
+SELECT CAST(ROUND(AVG(0.3*50*price),2) as DECIMAL(5,2)) as 'Profit'
+FROM menu
+
 -- Task 2
 
 --3
@@ -18,3 +32,22 @@ FROM menu;
 --4
 
 --5
+
+--6
+SELECT recipe.ingredient, menu.pizza from MENU
+LEFT JOIN recipe ON menu.pizza=recipe.pizza
+LEFT JOIN items ON recipe.ingredient=items.ingredient
+WHERE items.type='fish'
+ORDER BY recipe.ingredient ASC;
+
+--7
+SELECT DISTINCT type from menu
+LEFT JOIN recipe ON menu.pizza=recipe.pizza
+LEFT JOIN items ON recipe.ingredient=items.ingredient
+WHERE menu.country IN ('U.S','Mexico','Canada')
+
+--8
+SELECT menu.pizza FROM menu
+LEFT JOIN recipe ON menu.pizza=recipe.pizza
+LEFT JOIN items ON recipe.ingredient=items.ingredient
+WHERE menu.base='wholemeal' AND items.type='fruit'
