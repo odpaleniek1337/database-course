15ae97012a7de921fc2eafb502f46770ae156681
Iga <34031916+wojcikiga@users.noreply.github.com>
2021-04-23 21:00:00 +0200

    Update pizza_queries_2.sql
    task 1: 1,2,3 task2:1,2

--- pizza_queries_2.sql
+++ pizza_queries_2.sql
@@ -1,5 +1,18 @@
 -- Task 1
 
+--1
+SELECT COUNT (pizza) 
+FROM menu;
+
+--2
+SELECT COUNT (DISTINCT country) 
+FROM menu;
+
+--3
+SELECT MIN(price) AS "Cheapest_Italian"
+FROM menu
+WHERE country='Italy'
+
 --4
 SELECT SUM(price) AS margherita_vegetarian
 FROM menu
@@ -26,7 +39,16 @@ SELECT CAST(ROUND(AVG(0.3*50*price),2) as DECIMAL(5,2)) as 'Profit'
 FROM menu
 
 -- Task 2
+                                                  
+--1
+SELECT recipe.ingredient, items.type
+from recipe
+INNER JOIN items ON items.ingredient= recipe.ingredient WHERE recipe.pizza='Margherita'
 
+--2
+SELECT items.ingredient, recipe.pizza
+from recipe
+INNER JOIN items ON items.ingredient= recipe.ingredient	WHERE items.type='fish'
 --3
 
 --4
