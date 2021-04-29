77026533105dd4e5d3b301ecebbfc410a0af7d75
odpaleniek1337 <deadlodz@wp.pl>
2021-04-29 22:01:42 +0200

    we tryin to do 2.3 xd

--- pizza_queries_3.sql
+++ pizza_queries_3.sql
@@ -10,17 +10,18 @@ GROUP BY country;
 SELECT country, MAX(price) as 'Highest price' FROM menu
 WHERE country IS NOT NULL
 GROUP BY country;
+
 --3
-SELECT country, MIN(price) as 'min price' 
-FROM menu
+SELECT country, MIN(price) as 'min price' FROM menu
 WHERE country IS NOT NULL
 GROUP BY country;
- --4
- SELECT country, AVG(price) as 'avg price'
- FROM menu
- WHERE country IS NOT NULL 
- group by country
- having count(pizza)>1 
+
+--4
+SELECT country, AVG(price) as 'avg price' FROM menu
+WHERE country IS NOT NULL 
+group by country
+having count(pizza)>1 
+
 --5
 SELECT country, CAST(AVG(price) as DECIMAL(3,2)) as 'Average price' FROM menu
 WHERE country LIKE '%i%'
@@ -39,16 +40,46 @@ SELECT pizza, price FROM menu
 WHERE price >
 (SELECT MAX(price) FROM menu WHERE country='Italy')
 
+
 --2
-SELECT pizza FROM recipe
+SELECT DISTINCT pizza FROM recipe
 WHERE ingredient IN
 (SELECT ingredient FROM items WHERE type='meat')
 
+
 --3
-/*SELECT ingredient, pizza, amount FROM recipe
+SELECT ingredient, pizza, amount FROM recipe
+Where amount = (SELECT MAX(amount) FROM recipe)
 GROUP BY ingredient
-HAVING amount = (SELECT MAX(amount) FROM recipe)
-Ÿle jest ale nie wiem jak zrobiæ tbh*/
+--Ÿle jest ale nie wiem jak zrobiæ tbh
+
+SELECT ingredient, pizza, amount FROM recipe
+SELECT MAX(amount) FROM recipe GROUP BY ingredient
+SELECT ingredient FROM recipe GROUP BY ingredient
+
+--3
+select recipe.ingredient, recipe.pizza, recipe.amount
+from recipe
+inner join
+(select  ingredient, max(amount) amt
+from recipe
+group by ingredient) rcp
+on recipe.ingredient = rcp.ingredient and recipe.amount = rcp.amt
+order by recipe.ingredient desc
+
+--4
+SELECT ingredient FROM recipe rec
+WHERE (SELECT count(pizza) FROM recipe WHERE ingredient = rec.ingredient) > 1
+GROUP BY ingredient
+
+--5
+SELECT pizza FROM recipe
+WHERE ingredient IN(SELECT ingredient FROM items WHERE type='meat')
+GROUP BY pizza
+
+--6
+SELECT ingredient FROM items
+WHERE ingredient NOT IN (SELECT ingredient FROM recipe)
 
 --7
 SELECT ingredient FROM recipe rec
