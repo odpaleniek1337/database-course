0efff629d8eae50c1fc3404f60b6e7deba01e2a4
Lidia <230459@edu.p.lodz.pl>
2021-04-29 18:34:12 +0200

    task 2

--- pizza_queries_3.sql
+++ pizza_queries_3.sql
@@ -1,4 +1,5 @@
 --Task 1 
+USE pizza;
 
 --1
 SELECT country, CAST(AVG(price) as DECIMAL(3,2)) as 'Average price' FROM menu
@@ -23,6 +24,22 @@ HAVING min(price) < 7.50;
 
 --Task 2
 
+--1
+SELECT pizza, price FROM menu
+WHERE price >
+(SELECT MAX(price) FROM menu WHERE country='Italy')
+
+--2
+SELECT pizza FROM recipe
+WHERE ingredient IN
+(SELECT ingredient FROM items WHERE type='meat')
+
+--3
+/*SELECT ingredient, pizza, amount FROM recipe
+GROUP BY ingredient
+HAVING amount = (SELECT MAX(amount) FROM recipe)
+Ÿle jest ale nie wiem jak zrobiæ tbh*/
+
 --7
 SELECT ingredient FROM recipe rec
 WHERE (SELECT count(pizza) FROM recipe WHERE ingredient = rec.ingredient) > 1
@@ -39,7 +56,7 @@ GROUP BY pizza
 HAVING count(rec.ingredient) = (SELECT MAX(ing) FROM (SELECT pizza, count(ingredient) ing FROM recipe GROUP BY pizza) as aa)
 
 --10
-SELECT DISTINCT type from items it
+SELECT DISTINCT type FROM items it
 WHERE it.ingredient IN (SELECT ingredient FROM recipe rec
 WHERE rec.pizza = (SELECT pizza FROM menu m
 WHERE (SELECT max(price) FROM menu) = m.price))
