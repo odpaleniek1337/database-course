69539b453c686a79bd3896443b4fe9af5a102be7
Iga <34031916+wojcikiga@users.noreply.github.com>
2021-05-16 00:28:14 +0200

    Update MDMFurnitures_queries_02.sql
    1-10 queries :)

--- furniture/MDMFurnitures_queries_02.sql
+++ furniture/MDMFurnitures_queries_02.sql
@@ -1,5 +1,64 @@
 USE PVFC
 
+--1
+select CustomerName from customer_t
+where customerid not in (select CustomerID from Order_T)
+
+--2
+select ProductLineID, count(product_t.productid) as numberofproducts , avg( Product_T.ProductStandardPrice) as avgproductprice
+from  Product_T
+where productid= Product_T.ProductID 
+group by ProductlineID;
+
+--3
+select ProductLineID, count(product_t.productid) as numberofproducts , avg( Product_T.ProductStandardPrice) as avgproductprice
+from  Product_T
+where productid= Product_T.ProductID 
+group by ProductlineID
+having avg( Product_T.ProductStandardPrice)>200;
+
+--4
+select employeesupervisor, count(employeename) as headcount
+from Employee_T 
+group by EmployeeSupervisor
+having count(employeename)>2 ;
+
+--5
+--nie mam pojecia skad wziac manager's birth, name i tak dalej :/ bo w tej tabeli employee_T nie ma nic o menagerze
+
+--6
+
+select order_t.orderid, order_t.CustomerID, order_t.OrderDate, Product_T.ProductDescription
+from ((Order_T 
+inner join OrderLine_T on order_t.OrderID= OrderLine_T.OrderID)
+inner join Product_T on OrderLine_T.ProductID= Product_T.ProductID)
+where Order_T.CustomerID=3;
+
+--7
+select Product_T.ProductID, Product_T.ProductStandardPrice, sum(Product_T.ProductStandardPrice * orderline_t.orderedquantity) as totalprice
+from product_T
+inner join OrderLine_T on Product_T.ProductID=OrderLine_T.ProductID
+where orderline_t.OrderID=1 group by Product_T.ProductID, Product_T.ProductStandardPrice
+
+
+--8
+select workcenterid, count( worksin_t.employeeid)
+from WorksIn_T
+where workcenterid=worksin_t.WorkCenterID
+group by workcenterid
+--tylko nie ma tego trzecieko centerid nie wiem czemu
+
+--9
+select workcenterid from WorksIn_T
+inner join EmployeeSkills_T on worksin_t.EmployeeID= EmployeeSkills_T.EmployeeID 
+where EmployeeSkills_T.SkillID='QC1'
+
+--10
+
+select sum(Product_T.ProductStandardPrice) as totalcost
+from Product_T
+where productid in (select productid from orderline_t where orderid=1)
+
 --21
 select OrderID from Order_T
 except select OrderID from Payment_T
